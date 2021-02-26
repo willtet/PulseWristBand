a83757c1776e4275727f5649f8a5d577d4bad1f9
Willian Takashi <williantet@gmail.com>
2021-02-26 20:23:03 -0300

    Initial commit

---
+++ app/src/main/java/com/newhorizon/pulsewristband/MainActivity.java
@@ -0,0 +1,26 @@
+package com.newhorizon.pulsewristband;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword("willian@email.com","123456");
+    }
+
+
+}

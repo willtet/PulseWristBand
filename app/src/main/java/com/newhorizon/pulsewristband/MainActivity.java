8990b84b377804a4fceff114b0701bccb5f9b360
Willian <willian0404@hotmail.co.jp>
2021-03-17 19:29:11 -0300

    teste de bluetooth

--- app/src/main/java/com/newhorizon/pulsewristband/MainActivity.java
+++ app/src/main/java/com/newhorizon/pulsewristband/MainActivity.java
@@ -2,25 +2,85 @@ package com.newhorizon.pulsewristband;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHealth;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    Button btn;
+    BluetoothHealth bh;
+    int REQUEST_ENABLE_BT = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.bt_entrar);
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.createUserWithEmailAndPassword("willian@email.com","123456");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!adapter.isEnabled()){
+                    Intent enable = new Intent(adapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enable, REQUEST_ENABLE_BT);
+                }
+                // Register for broadcasts when a device is discovered.
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(receiver, filter);
+
+                Intent discoverableIntent =
+                        new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+
+
+            }
+        });
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
 
 
 }

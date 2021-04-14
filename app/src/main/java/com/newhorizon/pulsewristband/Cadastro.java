d4a971e71221895ee941ee230b135412c277458f
Willian Takashi <williantet@gmail.com>
2021-04-14 00:39:13 -0300

    Tentando fazer funcionar

--- app/src/main/java/com/newhorizon/pulsewristband/Cadastro.java
+++ app/src/main/java/com/newhorizon/pulsewristband/Cadastro.java
@@ -3,12 +3,55 @@ package com.newhorizon.pulsewristband;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Cadastro extends AppCompatActivity {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private EditText nome;
+    private EditText email;
+    private EditText senha;
+    private EditText confirmar;
+    private RadioGroup grupo;
+    private RadioButton escolhido;
+    private Button cadastrar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nome = findViewById(R.id.cadastro_nome);
+        email = findViewById(R.id.cadastro_email);
+        senha = findViewById(R.id.cadastro_senha);
+        confirmar = findViewById(R.id.cadastro_confirmar);
+
+
+        cadastrar = findViewById(R.id.bt_cadastrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (senha.toString().equals(confirmar.toString())){
+
+                }
+            }
+        });
+
+    }
+
+    public void checkedButton(View v){
+        int id = grupo.getCheckedRadioButtonId();
+        escolhido = findViewById(id);
+
     }
 }
--- app/src/main/java/com/newhorizon/pulsewristband/MainActivity.java
+++ app/src/main/java/com/newhorizon/pulsewristband/MainActivity.java
@@ -18,76 +18,20 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    Button btn;
-    BluetoothHealth bhk;
-    int REQUEST_ENABLE_BT = 1;
-
-
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.bt_entrar);
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!adapter.isEnabled()){
-                    Intent enable = new Intent(adapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enable, REQUEST_ENABLE_BT);
-                }
-                // Register for broadcasts when a device is discovered.
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                registerReceiver(receiver, filter);
-
-                Intent discoverableIntent =
-                        new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-                startActivity(discoverableIntent);
-
-
-            }
-        });
     }
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-
-            Intent discoverableIntent =
-                    new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-            startActivity(discoverableIntent);
-
-
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver);
-    }
-
 
 
 }
